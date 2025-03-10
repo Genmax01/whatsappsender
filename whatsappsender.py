398478657828e0049fbd86da14247cbb4c50fd8a
Genmax01 <202567395+Genmax01@users.noreply.github.com>
2025-03-10 02:29:14 +0100

    First commit

--- whatsappsender.py
+++ whatsappsender.py
@@ -19,7 +19,7 @@ class WhatsappSender:
     def __init__(self):
         super().__init__()
         self.options = Options()
-        self.pathToUserData='./UserDataProfile'
+        self.pathToUserData='UserDataProfile'
         self.userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
         self.driver = None
         self.firstRun = False
